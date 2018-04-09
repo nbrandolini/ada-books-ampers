2d25a4f1945d3a27811f6771850ea910fab0af85
CheezItMan <chris@adadevelopersacademy.org>
2018-04-09 11:18:42 -0700

    Book tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,13 +4,13 @@ require "rails/test_help"
 require "minitest/rails"
 require "minitest/reporters"  # for Colorized output
 
+
 #  For colorful output!
 Minitest::Reporters.use!(
   Minitest::Reporters::SpecReporter.new,
   ENV,
   Minitest.backtrace_filter
 )
-
 
 # To add Capybara feature tests add `gem "minitest-rails-capybara"`
 # to the test group in the Gemfile and uncomment the following:
